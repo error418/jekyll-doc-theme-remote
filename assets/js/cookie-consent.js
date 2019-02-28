85ca893170c3691923a4afceeb711af2e53073bc
error418 <error418@users.noreply.github.com>
2019-02-28 09:44:11 +0100

    use correct path config option

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -34,7 +34,7 @@ window.addEventListener("load", function() {
       }
     },
     "cookie": {
-      "site": "{{ site.baseurl }}"
+      "path": "{{ site.baseurl }}"
     },
     "type": "opt-in",
     "content": {
